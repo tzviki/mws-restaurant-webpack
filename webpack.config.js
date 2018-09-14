5c80e54893af6f376f7fc8137123ff2ce563305c
Tzviki Wolvovsky <tzviki@rallybound.com>
2018-09-14 13:42:28 -0400

    added babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
